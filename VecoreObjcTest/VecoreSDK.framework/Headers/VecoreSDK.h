8b57a820ba52f3fd72862875f0c5fc20b7c23150
Mattijs Fuijkschot <mattijs@innovactory.com>
2020-03-04 16:00:31 +0100

    Added / Embedded VecoreSDK

---
+++ VecoreObjcTest/VecoreSDK.framework/Headers/VecoreSDK.h
@@ -0,0 +1,17 @@
+//
+//  StappinSDK.h
+//  StappinSDK
+//
+//  Created by Ramon Swilem on 27/08/2019.
+//  Copyright © 2019 Ramon Swilem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StappinSDK.
+FOUNDATION_EXPORT double StappinSDKVersionNumber;
+
+//! Project version string for StappinSDK.
+FOUNDATION_EXPORT const unsigned char StappinSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StappinSDK/PublicHeader.h>
